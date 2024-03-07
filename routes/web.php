799684771bdaa45dfe55671ba8540410a4349e4d
Ratan Mia <ratan.kawasaki@gmail.com>
2024-03-07 19:03:40 +0600

    Adding Routers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
+
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
     return view('welcome');
 })->middleware(['verify.shopify'])->name('home');
@@ -20,3 +24,8 @@ Route::get('/', function () {
 Route::get('/{path?}', function () {
     return view('welcome');
 })->middleware(['verify.shopify'])->where('path', '.*');
+
+Route::get('/', [\App\Http\Controllers\ProductController::class, 'index'])->middleware(['verify.shopify'])->name('home');
+
+Route::get('/products', [\App\Http\Controllers\ProductController::class, 'index'])
+    ->middleware(['verify.shopify'])->name('product.index');
